819c8b6c5eae07511875822ef612782b97089564
jwepdx <37279570+jwepdx@users.noreply.github.com>
2020-01-18 15:53:50 -0800

    add initial files for testing

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const httpsOptions = {
+  key: fs.readFileSync(process.env.KEY_FILE),
+  cert: fs.readFileSync(process.env.CERT_FILE),
+};
+const server = require("https").createServer(httpsOptions, app);
+
+// Webserver Config
+const routes = require("./lib/routes/index.js");
+const errorHandler = require("./lib/app-errorhandler.js");
+require("./lib/app-settings.js")(app);
+require("./lib/app-middleware.js")(app);
+// Routing
+app.get("/", routes.main.index);
+
+app.use(errorHandler.notFound);
+app.use(errorHandler.serverError);
+
+// Socket.IO Config
+require("./lib/socket.js")(server);
+
+// Listen on port
+const port = process.env.PORT || 443;
+server.listen(port, function() {
+  console.log(`Server listening on ${port}`);
+});
