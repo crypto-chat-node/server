6020f77a9eff71d40b1e36f1490634b31693d012
jwepdx <37279570+jwepdx@users.noreply.github.com>
2020-01-18 20:56:34 -0800

    Add view outlines and finish get routes

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
+"use strict";
+// Load Enviorment Config
 require("dotenv").config();
+// Load required modules
+const path = require("path");
 const express = require("express");
-const app = express();
 const fs = require("fs");
+const routes = require("./lib/routes/index.js");
+const authHandler = require("./lib/auth-middleware");
+const errorHandler = require("./lib/app-errorhandler.js");
+
+// Set HTTPS Options
 const httpsOptions = {
   key: fs.readFileSync(process.env.KEY_FILE, "utf-8"),
   cert: fs.readFileSync(process.env.CERT_FILE, "utf-8"),
   ca: fs.readFileSync(process.env.CA_FILE, "utf-8")
 };
+
+// Set authHandlerOptions
+const authHandlerOptions = {};
+
+// Get Auth Function
+const authFunction = authHandler(authHandlerOptions);
+
+// Initialize App
+const app = express();
+
+// Create Webservers
 const httpServer = require("http").createServer(app);
 const server = require("https").createServer(httpsOptions, app);
 
 // Webserver Config
-const routes = require("./lib/routes/index.js");
-const errorHandler = require("./lib/app-errorhandler.js");
 require("./lib/app-settings.js")(app);
 require("./lib/app-middleware.js")(app);
+
+// Static Resource Serving
+app.use("/jquery", express.static(path.join(__dirname, "node_modules/jquery/dist")));
+app.use("/bootstrap", express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
+app.use("/popper.js", express.static(path.join(__dirname, "node_modules/popper.js/dist")));
+
 // Routing
 app.get("/", routes.main.index);
+app.get("/about", routes.main.about);
+app.get("/signin", routes.user.signin);
+app.post("/signin", routes.user.signin.post);
+app.get("/signup", routes.user.signup);
+app.post("/signup", routes.user.signup.post)
 
+app.get("/profile", authFunction, routes.user.profile);
+app.post("/profile", authFunction, routes.user.profile.post);
+app.get("/settings", authFunction, routes.user.settings);
+app.post("/settings", authFunction, routes.user.settings.post);
+app.get("/chats", authFunction, routes.chat.chats);
+app.post("/chats/new", authFunction, routes.chat.new);
+
+
+// Error Handling Routes
 app.use(errorHandler.notFound);
 app.use(errorHandler.serverError);
 
